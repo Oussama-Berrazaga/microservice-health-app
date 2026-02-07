8b5b2e69466b063a7416e49f83f37db3a84844dd
Oussama Berrazaga <oussema.berrazaga@gmail.com>
2026-02-07 11:05:13 +0100

    feat: Implement ErrorBoundary component for improved error handling in the application

---
+++ dashboard-ui/src/components/ErrorBoundary.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+interface ErrorBoundaryProps {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+}
+
+export default class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, info: { componentStack: any }) {
+    logErrorToMyService(
+      error,
+      // Example "componentStack":
+      //   in ComponentThatThrows (created by App)
+      //   in ErrorBoundary (created by App)
+      //   in div (created by App)
+      //   in App
+      info.componentStack,
+      // Warning: `captureOwnerStack` is not available in production.
+      React.captureOwnerStack(),
+    );
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+function logErrorToMyService(
+  error: any,
+  componentStack: any,
+  arg2: string | null,
+) {
+  throw new Error("Function not implemented.");
+}
--- dashboard-ui/src/main.tsx
+++ dashboard-ui/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary fallback={<div>Something went wrong.</div>}>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
-)
+);
